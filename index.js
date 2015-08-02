04fb5b556e68139b52e7229e8ca1bff8c1bfb125
Diego Celis <dcelis29@gmail.com>
2015-08-02 15:59:27 -0500

    geolocation

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-function(){
+(function(){
 	if(navigator.geolocation){
 		navigator.geolocation.getCurrentPosition(getCoords, errorFounds);	
 	}else{
-		alert("Actualiza tu navegador");
+		alert("Actualiza tu navegador");	
 	}
-}
 
-function errorFound(error){
-	alert("Ocurrió un error: " + error.code);
-	//0: Error desconocido
-	//1: Permiso denegado
-	//2: Posición no está disponible
-	//3: Timeout
-};
+	function errorFound(error){
+			alert("Ocurrió un error: " + error.code);
+		//0: Error desconocido
+		//1: Permiso denegado
+		//2: Posición no está disponible
+		//3: Timeout
+	};
 
-function getCoords(position){
-	var lat = position.coords.latitude;
-	var lon = position.coords.longitude;
-	console.log("Tu posición es "+ lat + "," + lon);
-}
+	function getCoords(position){
+		var lat = position.coords.latitude;
+		var lon = position.coords.longitude;
+		console.log("Tu posición es "+ lat + "," + lon);
+	}
+})();
