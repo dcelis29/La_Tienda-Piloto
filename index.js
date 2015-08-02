ca105133636c3cd569e70272c74bc0beb670166b
Diego Celis <dcelis29@gmail.com>
2015-08-02 15:45:05 -0500

    Geolocation

---
+++ index.js
@@ -0,0 +1,21 @@
+(function()){
+	if(navigator.geolocation){
+		navigator.geolocation.getCurrentPosition(getCoords, errorFounds);	
+	}else{
+		alert("Actualiza tu navegador");
+	}
+}
+
+function errorFound(error){
+	alert("Ocurrió un error: " + error.code);
+	//0: Error desconocido
+	//1: Permiso denegado
+	//2: Posición no está disponible
+	//3: Timeout
+};
+
+function getCoords(position){
+	var lat = position.coords.latitude;
+	var lon = position.coords.longitude;
+	console.log("Tu posición es "+ lat + "," + lon);
+}
